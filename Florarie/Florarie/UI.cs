6e4e415770a2dd8dc793ceb6b0444a33090e5739
AdrianZemora04 <adrian.zemora@student.upt.ro>
2025-01-06 19:15:31 +0200

    actualizare metoda logare

--- Florarie/Florarie/UI.cs
+++ Florarie/Florarie/UI.cs
@@ -23,11 +23,62 @@ public class UI
         if (currentUser.cod.StartsWith('a'))
         {
             // angajat functionalities
-            Console.WriteLine("dsadadasda");
+            string cod1;
+            Console.WriteLine("Angajat logat");
+            Console.WriteLine("1 - Viziualizare comenzi");
+            Console.WriteLine("2 - Comanda materie pentru buchete");
+            Console.WriteLine("3 - Preluare materie pentru buchete");
+            Console.WriteLine("4 - Preluare comanda buchet");
+            Console.WriteLine("5 - Finalizare comanda");
+            Console.WriteLine("Anything else - Exit");
+            Console.WriteLine("Introduceti optiunea");
+            cod1 = Console.ReadLine();
+            switch (cod1)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;  
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                default: Console.WriteLine("Delogare reusita");
+                    runApp(); 
+                    break;
+            }
         }
         else
         {
             // client functionalities
+            string cod2;
+            Console.WriteLine("Client logat");
+            Console.WriteLine("1 - Comanda de buchet");
+            Console.WriteLine("2 - Vizualizare istoric comenzi");
+            Console.WriteLine("3 - Vizualizare detalii comanda");
+            Console.WriteLine("4 - Ridicare comanda");
+            Console.WriteLine("5 - Review comanda");
+            Console.WriteLine("Anything else - Exit");
+            Console.WriteLine("Introduceti optiunea");
+            cod2 = Console.ReadLine();
+            switch (cod2)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;  
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                default: Console.WriteLine("Delogare reusita");
+                    runApp(); 
+                    break;
+            }
         }
     }
 
@@ -45,12 +96,40 @@ public class UI
                 this.currentUser = currentUser;
                 mainApp();
             }
-            else
+            else if (currentUser == null)
             {
-                Console.WriteLine("The code has no associated user");
+                bool logged = false;
+                while (!logged)
+                {
+                    Console.WriteLine("1 - Enter unique code again: ");
+                    Console.WriteLine("Anything else - Back to main menu");
+                    string userInput1 = Console.ReadLine();
+                
+                    if (userInput1.Equals("1"))
+                    {
+                        Console.WriteLine("Enter unique code: ");
+                        string userInput2 = Console.ReadLine();
+                        currentUser = userService.findOne(userInput2);
+                        if (currentUser != null)
+                        {
+                            loggedIn = true;
+                            logged = true;
+                            this.currentUser = currentUser;
+                            mainApp();
+                        }
+                    }
+                    else
+                    {
+                        runApp();
+                        logged = true;
+                    }
+
+                }
+                
             }
         }
     }
+    
 
     private void handleRegister()
     {
@@ -75,6 +154,7 @@ public class UI
 
     public void runApp()
     {
+        
         bool stillRunning = true;
         while (stillRunning)
         {
@@ -84,10 +164,12 @@ public class UI
             if (userInput.Equals("1"))
             {
                 handleLogin();
+                stillRunning = false;
             }
             else if (userInput.Equals("2"))
             {
                 handleRegister();
+                stillRunning = false;
             }
             else
             {
