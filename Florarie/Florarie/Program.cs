3f7bbaa57de0acb283a64c3a44e7ad357b90f5e7
Sergiu-Andrei-B <sergiu.boboiciov@student.upt.ro>
2025-01-13 14:15:31 +0200

    Rezolvat conflictele

--- Florarie/Florarie/Program.cs
+++ Florarie/Florarie/Program.cs
@@ -1,20 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Florarie;
-using System;
-using System.IO;
 
 public static class Program
 {
     public static void Main()
     {
-        
-        UserRepo repo = new UserRepo("D:\\Faculta\\Anul 2\\Semestrul 1\\POO\\ProiectFlorarie\\Florarie\\Florarie\\user.txt");
+        UserRepo repo = new UserRepo("E:\\clonaFlorarie\\Florarie\\Florarie\\user.txt");
         UserService service = new UserService(repo);
         GestionareComenzi gestionareComenzi = new GestionareComenzi();
         Meniu meniu = new Meniu(service, gestionareComenzi);
-        
-        meniu.startMeniu();
-        
+        meniu.runApp();
     }
 }
